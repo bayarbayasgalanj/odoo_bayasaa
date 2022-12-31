6e4688159d27e032f6e453aa07fa6ce7508cd77f
Bayasaa <bayarbayasgalan.j@gmail.com>
2022-12-31 11:48:23 -0800

    BB Stock quant report and extra tools initial module commit

---
+++ bb_stock_quant_report/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'BB Stock quant report and extra tools',
+    'version': '1.0.1',
+    'category': 'Stock',
+    'icon': '/bb_stock_quant_report/static/description/icon.png',
+    'sequence': 20,
+    'author': 'Bayarbayasgalan MGL',
+    'summary': 'Stock Extra tools',
+    'description': """
+This module can show Stock available quantity details with Cost and Sales Price
+==============================================
+If you want to show Stock Balance with cost and Sale.
+You should take Stock Administrator at the user.
+Cost is comfortable Average cost and Standard cost.
+    """,
+    'depends': ['stock','product'],
+    'data': [
+        'security/bb_stock_security.xml',
+        'security/ir.model.access.csv',
+        'views/stock_quant_view.xml',
+        'views/stock_quant_report_view.xml',
+    ],
+    'qweb': [],
+    'website': 'https://www.linkedin.com/in/bayarbayasgalan-jagdal/',
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
